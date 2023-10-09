11d090fe88c376402f643a831b5be6a42bb65cbe
PaleLuna <robertmkrtyan334@gmail.com>
2023-10-09 19:17:55 +0300

    Инициализация проекта

---
+++ Sea-Battle/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Sea_Battle
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
